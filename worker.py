a354b1c3272dc254fffca9710a9f76a40da2a83d
Leandro de Lima Camargo <leandro@kcane.co>
2025-12-30 10:18:44 -0300

    first working version

--- worker.py
+++ worker.py
@@ -1,7 +1,12 @@
 from scapy.all import *
 import time
 import json
-from statistics import stdev, median
+
+### Configuration
+# IP for the test
+ip_target = '1.1.1.1'
+
+###
 
 
 def packet_builder(ip_dst, ip_ttl):
@@ -34,124 +39,93 @@ def packet_sender(pkt):
                 break
         else:
             if unreachable_hops_counter == 3:
-                break
+                return 'Destination unreachable'
             unreachable_hops_counter += 1
             packet_path[f'Hop #{pkt.ttl} unreachable'] = {
                 "ttl": None,
                 "latency": f'{end_time:.2f}'
             }
 
-        #packet_analyser(pkt_response, end_time)
+        # Increase TTL to go to next hop
         pkt.ttl += 1        
-
-
-
-        # Stops if 3 hops are unreachable
-        
-
-        # Stops when reach the 10 hop
+      
+        # Stops when reach the 30 hop
         if pkt.ttl == 31:
             break
 
     return packet_path
 
 
-def are_paths_same(network_tests):
-    is_same_path = True
+def same_path_detection(network_tests):
+    different_path = None
     for network_test in network_tests[1:]:
-        if not 'unreachable' in network_test.keys():
+        if not 'unreachable' in str(network_test.keys()):
             if network_tests[0].keys() != network_test.keys():
-                is_same_path = False
+                different_path = True
                 break
+    return different_path   
 
-    return is_same_path   
 
-
-def loss_detection(network_tests):
-    hop_counter = dict()
+def packet_loss_detection(network_tests):
     detected_packet_loss = dict()
-
-    for network_test in network_tests:
-        for hop in network_test.keys():
+    hop_counter = dict()
+    for traceroute in network_tests:
+        for hop in traceroute.keys():
             if 'unreachable' in hop:
                 continue
             hop_counter[hop] = hop_counter.get(hop, 0) + 1
     
     for hop in hop_counter:
-        if hop_counter[hop] < 5:
-            detected_packet_loss[hop] = f'{(hop_counter[hop] / 5) * 100}%'
+        if hop_counter[hop] < 3:
+            detected_packet_loss[hop] = f'{100 - ((hop_counter[hop] / 5) * 100)}%'
         
     if detected_packet_loss:
         return detected_packet_loss
-    
-    return 'No loss detected'
+
 
 
 def high_latency_detection(network_tests):
     high_latency = dict()
+    high_latency_counter = dict()
     for trace in network_tests:
         for hop in trace:
-            if float(trace[hop]['latency']) > 100:
-                high_latency[hop] = trace[hop]['latency']
+            if not 'unreachable' in hop and float(trace[hop]['latency']) > 100:
+                high_latency_counter[hop] = high_latency_counter.get(hop, 0) + 1
+                if high_latency_counter[hop] > 3:
+                    high_latency[hop] = trace[hop]['latency']
     if high_latency:
         return high_latency
-    
-    return 'No high latency detected'
+
 
 
 if __name__ == '__main__':
     networks_tests = []
     for i in range(1, 6):
-        pkt = packet_builder('1.1.1.1', 1)
-        networks_tests.append(packet_sender(pkt))
+        pkt = packet_builder(ip_target, 1)
+        trace_test = packet_sender(pkt)
+        if 'unreachable' not in trace_test:
+            networks_tests.append(trace_test)
         time.sleep(1)
-    #print(json.dumps(networks_tests))
-    print(f'Same paths: {are_paths_same(networks_tests)}')
-    print(f'Packet loss: {loss_detection(networks_tests)}')
-    print(f'High latency: {high_latency_detection(networks_tests)}')
-
-
-
-
-
-
-
-
-
-
-
-
-# def packet_crafter(pkt_dst, pkt_ttl):
-#     unreachable_hops_counter = 0
-#     while True:
-#         ip_header = IP(dst=pkt_dst, ttl=pkt_ttl)
-#         protocol_header = ICMP()
-#         pkt = ip_header / protocol_header
-
-#         try:
-#             send_time = time.time()
-#             pkt_response = sr1(pkt, timeout=1, verbose=0)
-#             receive_time = (time.time() - send_time) * 1000
-#             if pkt_response is None:
-#                 print(f"{pkt_ttl}\t???\t100.0")
-#                 unreachable_hops_counter += 1
-#                 pkt_ttl += 1
-#             elif pkt_response.type == 0:
-#                 print(f"{pkt_ttl}\t{pkt_response.src}\t{receive_time:.2f} ms")
-#                 break
-#             elif pkt_response.type == 3:
-#                 print(f"{pkt_ttl}\t{pkt_response.src}\t{receive_time:.2f} ms")
-#                 break
-#             else:
-#                 print(f"{pkt_ttl}\t{pkt_response.src}\t{receive_time:.2f} ms")
-#         except Exception as e:
-#             print(f'Error while trying to send packet {e}')
-        
-#         if unreachable_hops_counter == 3:
-#             break
-#         if pkt_ttl == 30:
-#             break
-#         pkt_ttl += 1
-
-
+    
+    if networks_tests:
+        try:
+            result_test = dict()
+            different_path = same_path_detection(networks_tests)
+            packet_loss = packet_loss_detection(networks_tests)
+            high_latency = high_latency_detection(networks_tests)
+            if different_path:
+                result_test["different_path"] = different_path
+            if packet_loss:
+                result_test["packet_loss"] = packet_loss
+            if high_latency:
+                result_test["high_latency"] = high_latency
+            if result_test:
+                print(result_test)
+        except Exception as e:
+            print(f'Error: {e}')
+    
+    ## ONLY FOR DEBUG PURPOSES
+    # print(json.dumps(networks_tests))
+    #else:
+        #result_test = 'Destination Unreachable!'
 
